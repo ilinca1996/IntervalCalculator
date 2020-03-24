e553e8c013fbbb0fc6f35b8c13a0dd04acb2d182
ilinca <tazlaoanu.ilinca@gmail.com>
2020-03-24 12:16:09 +0200

    initial commit

---
+++ IntervalCalculator/src/com/calculator/config/AppConfig.java
@@ -0,0 +1,5 @@
+package com.calculator.config;
+
+public class AppConfig {
+
+}
---
+++ IntervalCalculator/src/com/calculator/config/DispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.calculator.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {AppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
